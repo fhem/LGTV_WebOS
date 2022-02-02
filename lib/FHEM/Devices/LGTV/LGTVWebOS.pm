52208b2832a268b058cff9a42da3df24b1eef777
Marko Oldenburg <fhemdevelopment@cooltux.net>
2022-02-02 13:42:10 +0100

    Subject line (try to keep under 50 characters)
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- lib/FHEM/Devices/LGTV/LGTVWebOS.pm
+++ lib/FHEM/Devices/LGTV/LGTVWebOS.pm
@@ -263,7 +263,7 @@ sub Define {
 
     $modules{LGTV_WebOS}{defptr}{ $hash->{HOST} } = $hash;
 
-    readingsSingleUpdate( $hash, 'state', 'Initialized', 1 );
+    ::readingsSingleUpdate( $hash, 'state', 'Initialized', 1 );
 
     if ($init_done) {
         TimerStatusRequest($hash);
