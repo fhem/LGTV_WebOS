6bb3a5971c29aa28efe8a0ca6f309da79fa53908
Marko Oldenburg <fhemdevelopment@cooltux.net>
2023-05-16 04:08:38 +0200

    <type>[optional scope]: <description>
    [optional body]
    
    [Ticket: no]

--- lib/FHEM/Devices/LGTV/LGTVWebOS.pm
+++ lib/FHEM/Devices/LGTV/LGTVWebOS.pm
@@ -2,7 +2,7 @@
 #
 # Developed with VSCodium and richterger perl plugin.
 #
-#  (c) 2017-2022 Copyright: Marko Oldenburg (fhemdevelopment at cooltux dot net)
+#  (c) 2017-2023 Copyright: Marko Oldenburg (fhemdevelopment at cooltux dot net)
 #  All rights reserved
 #
 #   Special thanks goes to comitters:
