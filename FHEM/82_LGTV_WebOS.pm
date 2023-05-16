b812c8b9ba6c9e640c3e2b0148c4950157491c3a
Marko Oldenburg <fhemdevelopment@cooltux.net>
2023-05-16 04:16:43 +0200

    version change
    [Ticket: no]

--- FHEM/82_LGTV_WebOS.pm
+++ FHEM/82_LGTV_WebOS.pm
@@ -368,7 +368,7 @@ __END__
   ],
   "release_status": "stable",
   "license": "GPL_2",
-  "version": "v3.6.5",
+  "version": "v3.6.9",
   "author": [
     "Marko Oldenburg <fhemdevelopment@cooltux.net>"
   ],
